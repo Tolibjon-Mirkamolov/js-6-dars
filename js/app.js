d1ac99e690b75f9169368f69454e5fb09f1ccb30
Tolibjon <pubgdev150@gmail.com>
2022-03-21 16:53:20 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
 'use strict';
-// git hub
 
-// salom hamaga
+// let numberOfNews = +prompt("Siz qancha yangilik Ko'rdingiz");
+
+// const personalDB = {
+// 	count: numberOfNews,
+// 	news: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: true,
+// };
+
+// const a = prompt("Ohirgi Ko'rgan yangiliklarizdan biri"),
+// 	b = (numberOfNews = prompt('unga Qancha baho berar edingiz'));
+
+// personalDB.news[a] = b;
+
+// console.log(personalDB);
+
+let Tolibjon = prompt('salom tolibjon qancha dars kording');
+
+let answersDB = {
+	news: { Tolibjon },
+	cate: {},
+};
+
+const s = prompt('darslar sifati qanday '),
+	f = +prompt('darslarga qany darajada baxo berasiz');
+
+answersDB.cate[s] = f;
+
+console.log(answersDB);
